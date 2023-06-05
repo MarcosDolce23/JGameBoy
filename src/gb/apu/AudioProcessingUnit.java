dc7a9b4b85ccfd45066ce77875232ad024561b4c
Marcos Dolce <marcosdolce.91@gmail.com>
2023-06-05 20:06:38 -0300

    Order some attributes
    I ordered some attributes of the AudioProcessinUnit
    and added some coments. Not big changes

--- src/gb/apu/AudioProcessingUnit.java
+++ src/gb/apu/AudioProcessingUnit.java
@@ -4,16 +4,16 @@ import gb.Main;
 import gb.utils.BitOperations;
 
 public class AudioProcessingUnit {
-	private static AudioProcessingUnit instance;
-	
+    private static AudioProcessingUnit instance;
+
     // Sound buffer configuration
     private float[] bufferQueues = new float[AudioOutput.BUFFER_SIZE];
     private static byte[] buffer = new byte[AudioOutput.BUFFER_SIZE];
-	
-	// Buffer queue
-	private int bufferInd = 0;
-	
-    // =============== //   Sound Controller //
+
+    // Buffer queue
+    private int bufferInd = 0;
+
+    // =============== // Sound Controller //
     private int soundClocks = 0;
     private int soundInterval = Main.cpu.cyclesPerSec / 512; // 8192 cycles
 
@@ -22,83 +22,103 @@ public class AudioProcessingUnit {
 
     private boolean lengthStep = false;
     public boolean soundOn = false;
-    
+
     // Duty patterns
     private int[][] duty = {
-        {-1, -1, -1, -1, -1, -1, -1, 1}, // 12.5%
-        {1, -1, -1, -1, -1, -1, -1, 1}, // 25%
-        {1, -1, -1, -1, -1, 1, 1, 1}, // 50%
-        {-1, 1, 1, 1, 1, 1, 1, -1}  // 75%
+            { -1, -1, -1, -1, -1, -1, -1, 1 }, // 12.5%
+            { 1, -1, -1, -1, -1, -1, -1, 1 }, // 25%
+            { 1, -1, -1, -1, -1, 1, 1, 1 }, // 50%
+            { -1, 1, 1, 1, 1, 1, 1, -1 } // 75%
     };
-	
-	// Frequency and settings
+
+    // ======== Channel 1 ========
+
+    public boolean chan1On = false;
+
+    // Frequency and settings
     public boolean chan1CounterSelect = false;
     public int chan1InitFreq = 0;
     public int chan1RawFreq = 0;
-    
-    public boolean chan2CounterSelect = false;
-    public int chan2RawFreq = 0;
-    
-    public boolean chan3CounterSelect = false;
-    public int chan3RawFreq = 0;
-	
-	// Frequency timer
-    private int chan1FreqTimer = 0;
-    private int chan1DutyStep = 0;
-    
-    private int chan2FreqTimer = 0;
-    private int chan2DutyStep = 0;
-    
-    private int chan3FreqTimer = 0;
-    public int chan3SampleStep = 0;
-    
-    // Length and pattern duty
-    public int chan1PatternDuty = 0;
-    public int chan1Length = 0;
-    
-    public int chan2PatternDuty = 0;
-    public int chan2Length = 0;
-    
-    // Sweep register
-    public int chan1SweepTime = 0;
-    public boolean chan1SweepDec = false;
-    public int chan1SweepNum = 0;
 
+    // Frequency timer
+    private int chan1FreqTimer = 0;
+
+    // Length and pattern duty
+    public int chan1Length = 0;
+    public int chan1PatternDuty = 0;
+    private int chan1DutyStep = 0;
+
+    // Sweep register
     public boolean chan1SweepOn = false;
+    public boolean chan1SweepDec = false;
+    public int chan1SweepTime = 0;
+    public int chan1SweepNum = 0;
     private int chan1SweepClocks = 0;
-    
-    public float chan1EnvInit = 0;
+
+    // Channel envelope
+    public boolean chan1EnvOn = false;
     public boolean chan1EnvInc = false;
+    public float chan1EnvInit = 0;
     public int chan1EnvSweep = 0;
     public int chan1EnvClocks = 0;
-    
-    public float chan1EnvVol = 0; 
-    public boolean chan1EnvOn = false;
+    public float chan1EnvVol = 0;
     public float chan1EnvInterval = 0;
+
+    // ======== End Channel 1 ========
+
+    // ======== Channel 2 ========
+
+    public boolean chan2On = false;
+
+    // Frequency and settings
+    public boolean chan2CounterSelect = false;
+    public int chan2RawFreq = 0;
+
+    // Frequency timer
+    private int chan2FreqTimer = 0;
     
-    public float chan2EnvInit = 0;
+    // Length and pattern duty
+    public int chan2Length = 0;
+    public int chan2PatternDuty = 0;
+    private int chan2DutyStep = 0;
+
+    //  Channel envelope
+    public boolean chan2EnvOn = false;
     public boolean chan2EnvInc = false;
+    public float chan2EnvInit = 0;
     public int chan2EnvSweep = 0;
     public int chan2EnvClocks = 0;
-
     public float chan2EnvVol = 0;
-    public boolean chan2EnvOn = false;
     public float chan2EnvInterval = 0;
-    
-    public boolean chan1On = false;
-    public boolean chan2On = false;
+
+    // ======== End Channel 2 ========
+
+    // ======== Channel 3 ========
+
     public boolean chan3On = false;
+
+    // Frequency and settings
+    public boolean chan3CounterSelect = false;
+    public int chan3RawFreq = 0;
+
+    // Frequency timer
+    private int chan3FreqTimer = 0;
     
- // Volume shift
+    // Sample step
+    public int chan3SampleStep = 0;
+
+    // Volume shift
     public int chan3InitVolShift = 0;
     public int chan3VolShift = 4;
-    
- // Playback enable
+
+    // Playback enable
     public boolean chan3Playback = false;
-    
- // Length
+
+    // Length
     public int chan3Length = 0;
-	
+
+    // ======== End Channel 3 ========
+
     private int calcSweep() {
         int preFreq = chan1RawFreq;
         int newFreq = preFreq >> chan1SweepNum;
@@ -106,36 +126,36 @@ public class AudioProcessingUnit {
         newFreq = preFreq + (chan1SweepDec ? -newFreq : newFreq);
         return newFreq;
     }
-    
-	private boolean soundOn() {
-		return BitOperations.testBit(Main.mmu.getByte(0xff26), 7);
-	}
-	
+
+    private boolean soundOn() {
+        return BitOperations.testBit(Main.mmu.getByte(0xff26), 7);
+    }
+
     private void chan1Disable() {
         chan1On = false;
         chan1EnvVol = 0; // Mute
     }
-    
+
     private void chan2Disable() {
         chan2On = false;
         chan2EnvVol = 0; // Mute
     }
-    
+
     public void chan3Disable() {
         chan3On = false;
         chan3VolShift = 4;
     }
-    
-	private void chan1UpdateFreq(int cycles) {
+
+    private void chan1UpdateFreq(int cycles) {
         chan1FreqTimer -= cycles;
         if (chan1FreqTimer <= 0) {
-        	chan1FreqTimer += (2048 - chan1RawFreq) * 4;
+            chan1FreqTimer += (2048 - chan1RawFreq) * 4;
 
             chan1DutyStep++;
             chan1DutyStep &= 7;
         }
-	}
-	
+    }
+
     private void chan2UpdateFreq(int cycles) {
         chan2FreqTimer -= cycles;
         if (chan2FreqTimer <= 0) {
@@ -145,7 +165,7 @@ public class AudioProcessingUnit {
             chan2DutyStep &= 7;
         }
     }
-    
+
     private void chan3UpdateFreq(int cycles) {
         chan3FreqTimer -= cycles;
         if (chan3FreqTimer <= 0) {
@@ -155,7 +175,7 @@ public class AudioProcessingUnit {
             chan3SampleStep &= 0x1f;
         }
     }
-	
+
     private void chan1UpdateEnvelope() {
         chan1EnvClocks++;
 
@@ -166,19 +186,19 @@ public class AudioProcessingUnit {
 
             // Inc
             if (chan1EnvInc) {
-                chan1EnvVol += 1/15f;
+                chan1EnvVol += 1 / 15f;
                 if (chan1EnvVol > 1)
                     chan1EnvVol = 1;
             }
             // Dec
             else {
-                chan1EnvVol -= (1/15f);
+                chan1EnvVol -= (1 / 15f);
                 if (chan1EnvVol < 0)
                     chan1EnvVol = 0;
             }
         }
     }
-    
+
     private void chan2UpdateEnvelope() {
         chan2EnvClocks++;
 
@@ -189,19 +209,19 @@ public class AudioProcessingUnit {
 
             // Inc
             if (chan2EnvInc) {
-                chan2EnvVol += 1/15f;
+                chan2EnvVol += 1 / 15f;
                 if (chan2EnvVol > 1)
                     chan2EnvVol = 1;
             }
             // Dec
             else {
-                chan2EnvVol -= (1/15f);
+                chan2EnvVol -= (1 / 15f);
                 if (chan2EnvVol < 0)
                     chan2EnvVol = 0;
             }
         }
     }
-    
+
     private void chan1UpdateSweep() {
         if (--chan1SweepClocks > 0)
             return;
@@ -216,34 +236,32 @@ public class AudioProcessingUnit {
             if (!overflow && (chan1SweepNum > 0)) {
                 chan1RawFreq = newFreq;
 
-                if (calcSweep () > 2047)
+                if (calcSweep() > 2047)
                     chan1Disable();
-            }
-            else if (overflow)
+            } else if (overflow)
                 chan1Disable();
-        }
-        else
+        } else
             chan1SweepClocks = 8;
     }
-    
+
     private void chan1UpdateLength() {
         if (lengthStep && chan1CounterSelect && (--chan1Length == 0))
             chan1Disable();
     }
-    
+
     private void chan2UpdateLength() {
         if (lengthStep && chan2CounterSelect && (--chan2Length == 0))
             chan2Disable();
     }
-    
+
     private void chan3UpdateLength() {
         if (lengthStep && chan3CounterSelect && (--chan3Length == 0))
             chan3Disable();
     }
-    
-	public void chan1Trigger() {
+
+    public void chan1Trigger() {
         chan1On = true;
-        
+
         // Restart envelope
         chan1EnvVol = chan1EnvInit;
         chan1RawFreq = chan1InitFreq;
@@ -260,8 +278,8 @@ public class AudioProcessingUnit {
 
         if ((chan1SweepNum > 0) && (calcSweep() > 2047))
             chan1Disable();
-	}
-	
+    }
+
     public void chan2Trigger() {
         chan2On = true;
 
@@ -272,7 +290,7 @@ public class AudioProcessingUnit {
         if (chan2Length == 0)
             chan2Length = 64; // Full
     }
-    
+
     public void chan3Trigger() {
         if (!chan3Playback)
             return;
@@ -285,7 +303,7 @@ public class AudioProcessingUnit {
         if (chan3Length == 0)
             chan3Length = 256; // Full
     }
-	
+
     // Buffer methods
     private boolean stepBuffer() {
         float sample = getSample();
@@ -293,7 +311,7 @@ public class AudioProcessingUnit {
 
         return (bufferInd >= bufferQueues.length); // True when buffer(s) are full !
     }
-    
+
     private float getSample() {
         // Channel 1
         float sample1 = duty[chan1PatternDuty][chan1DutyStep] * chan1EnvVol;
@@ -306,7 +324,7 @@ public class AudioProcessingUnit {
         if (chan3VolShift == 4)
             sample3 = 0;
         else {
-        	int aux = 0;
+            int aux = 0;
             aux = chan3GetSample();
             aux = (aux + (aux - 0xf)) >> chan3VolShift;
             sample3 = aux / 15f;
@@ -315,44 +333,44 @@ public class AudioProcessingUnit {
         // Mix ...
         return (sample1 + sample2 + sample3) / 3;
     }
-    
+
     private int chan3GetSample() {
-    	if ((chan3SampleStep & 1) == 0) {
-    		return (((Main.mmu.ram[0xff30 + (chan3SampleStep >> 1)] & 0xff)	>> 4) & 0xf);
-    	}
+        if ((chan3SampleStep & 1) == 0) {
+            return (((Main.mmu.ram[0xff30 + (chan3SampleStep >> 1)] & 0xff) >> 4) & 0xf);
+        }
         return (((Main.mmu.ram[0xff30 + (chan3SampleStep >> 1)] & 0xff) & 0xf));
     }
-    
+
     private void copyBufferQueues() {
-        for (int i = 0; i < AudioOutput.BUFFER_SIZE; i ++)
+        for (int i = 0; i < AudioOutput.BUFFER_SIZE; i++)
             buffer[i] = (byte) (bufferQueues[i] * 127f);
     }
-    
-	public void handleSound(int cycles) {
-		if (!soundOn())
-			return;
-		
-		// Frequency Timmers
-		chan1UpdateFreq(cycles);
-		chan2UpdateFreq(cycles);
-		chan3UpdateFreq(cycles);
-		
-		// Every 512 hz ...
-		soundClocks += cycles;
-		if (soundClocks >= soundInterval) {
-			// Channel 1
+
+    public void handleSound(int cycles) {
+        if (!soundOn())
+            return;
+
+        // Frequency Timmers
+        chan1UpdateFreq(cycles);
+        chan2UpdateFreq(cycles);
+        chan3UpdateFreq(cycles);
+
+        // Every 512 hz ...
+        soundClocks += cycles;
+        if (soundClocks >= soundInterval) {
+            // Channel 1
             if (chan1On) {
                 chan1UpdateEnvelope();
                 chan1UpdateSweep();
                 chan1UpdateLength();
             }
-            
+
             // Channel 2
             if (chan2On) {
                 chan2UpdateEnvelope();
                 chan2UpdateLength();
             }
-            
+
             // Channel 3
             if (chan3On) {
                 chan3UpdateLength();
@@ -361,8 +379,8 @@ public class AudioProcessingUnit {
             lengthStep = !lengthStep;
 
             soundClocks -= soundInterval;
-		}
-		
+        }
+
         // Filling the buffer
         bufferClocks += cycles;
         if (bufferClocks >= bufferInterval) {
@@ -371,7 +389,7 @@ public class AudioProcessingUnit {
                 bufferInd = 0;
 
                 copyBufferQueues();
-                
+
                 // This action must be asynchronic
                 Thread threadPlayBuffer = new Thread(new ThreadPlayBuffer());
                 threadPlayBuffer.start();
@@ -379,22 +397,23 @@ public class AudioProcessingUnit {
 
             bufferClocks -= bufferInterval;
         }
-	}
-	
-	static class ThreadPlayBuffer implements Runnable {
-		@Override
-		public void run() {
-			AudioOutput audioOutput = AudioOutput.getInstance(buffer);
-			audioOutput.playBuffer();
-		}
-	}
-	
-	private AudioProcessingUnit( ) {}
-	
-	public static AudioProcessingUnit getInstance() {
-		if (instance == null) {
-			instance = new AudioProcessingUnit();
-		}
-		return instance;
-	}
+    }
+
+    static class ThreadPlayBuffer implements Runnable {
+        @Override
+        public void run() {
+            AudioOutput audioOutput = AudioOutput.getInstance(buffer);
+            audioOutput.playBuffer();
+        }
+    }
+
+    private AudioProcessingUnit() {
+    }
+
+    public static AudioProcessingUnit getInstance() {
+        if (instance == null) {
+            instance = new AudioProcessingUnit();
+        }
+        return instance;
+    }
 }
